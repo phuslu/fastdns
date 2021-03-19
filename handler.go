a5a0df4d0351820d3ed29ef5b988608441be4687
phuslu <phus.lu@gmail.com>
2021-03-20 03:40:15 +0800

    refactor handler

--- handler.go
+++ handler.go
@@ -1,5 +1,9 @@
 package fastdns
 
+import (
+	"net"
+)
+
 type Handler interface {
 	ServeDNS(rw ResponseWriter, req *Request)
 }
@@ -9,6 +13,28 @@ func Error(rw ResponseWriter, req *Request, code RCODE) {
 	defer ReleaseByteBuffer(b)
 
 	b.B = AppendRequestToResponse(b.B[:0], req, code, 0, 0, 0, 0)
+
+	_, _ = rw.Write(b.B)
+}
+
+func HostRecord(rw ResponseWriter, req *Request, ips []net.IP, ttl uint32) {
+	b := AcquireByteBuffer()
+	defer ReleaseByteBuffer(b)
+
+	b.B = b.B[:0]
+	b.B = AppendRequestToResponse(b.B, req, NOERROR, 1, Count(len(ips)), 0, 0)
+	b.B = AppendHostRecordsToResponse(b.B, req, ips, ttl)
+
+	_, _ = rw.Write(b.B)
+}
+
+func CNAMERecord(rw ResponseWriter, req *Request, cnames []string, ips []net.IP, ttl uint32) {
+	b := AcquireByteBuffer()
+	defer ReleaseByteBuffer(b)
+
+	b.B = b.B[:0]
+	b.B = AppendRequestToResponse(b.B, req, NOERROR, 1, Count(len(cnames)+len(ips)), 0, 0)
+	b.B = AppendCNameRecordsToResponse(b.B, req, cnames, ips, ttl)
 
 	_, _ = rw.Write(b.B)
 }
--- response.go
+++ response.go
@@ -78,3 +78,115 @@ func AppendRequestToResponse(dst []byte, req *Request, rcode RCODE, qd, an, ns, 
 
 	return dst
 }
+
+func AppendHostRecordsToResponse(dst []byte, req *Request, ips []net.IP, ttl uint32) []byte {
+	for _, ip := range ips {
+		if ip4 := ip.To4(); ip4 != nil {
+			_ = ip4[3]
+			answer := [...]byte{
+				// NAME
+				0xc0, 0x0c,
+				// TYPE
+				0x00, byte(QTypeA),
+				// CLASS
+				byte(req.Question.QClass >> 8), byte(req.Question.QClass),
+				// TTL
+				byte(ttl >> 24), byte(ttl >> 16), byte(ttl >> 8), byte(ttl),
+				// RDLENGTH
+				0x00, 0x04,
+				// RDATA
+				ip4[0], ip4[1], ip4[2], ip4[3],
+			}
+			dst = append(dst, answer[:]...)
+		} else {
+			_ = ip[15]
+			answer := [...]byte{
+				// NAME
+				0xc0, 0x0c,
+				// TYPE
+				0x00, byte(QTypeAAAA),
+				// CLASS
+				byte(req.Question.QClass >> 8), byte(req.Question.QClass),
+				// TTL
+				byte(ttl >> 24), byte(ttl >> 16), byte(ttl >> 8), byte(ttl),
+				// RDLENGTH
+				0x00, 0x10,
+				// RDATA
+				ip[0], ip[1], ip[2], ip[3],
+				ip[4], ip[5], ip[6], ip[7],
+				ip[8], ip[9], ip[10], ip[11],
+				ip[12], ip[13], ip[14], ip[15],
+			}
+			dst = append(dst, answer[:]...)
+		}
+	}
+
+	return dst
+}
+
+func AppendCNameRecordsToResponse(dst []byte, req *Request, cnames []string, ips []net.IP, ttl uint32) []byte {
+	offset := 12
+	// CName Records
+	for _, cname := range cnames {
+		answer := [...]byte{
+			// NAME
+			0xc0, byte(offset),
+			// TYPE
+			0x00, byte(QTypeCNAME),
+			// CLASS
+			byte(req.Question.QClass >> 8), byte(req.Question.QClass),
+			// TTL
+			byte(ttl >> 24), byte(ttl >> 16), byte(ttl >> 8), byte(ttl),
+			// RDLENGTH
+			0x00, byte(1 + len(cname) + 1),
+		}
+		dst = append(dst, answer[:]...)
+		// set offset
+		offset += len(dst)
+		// RDATA
+		dst = encodeDomain(dst, cname)
+	}
+	// Host Records
+	for _, ip := range ips {
+		if ip4 := ip.To4(); ip4 != nil {
+			_ = ip4[3]
+			answer := [...]byte{
+				// NAME
+				0xc0, byte(offset),
+				// TYPE
+				0x00, byte(QTypeA),
+				// CLASS
+				byte(req.Question.QClass >> 8), byte(req.Question.QClass),
+				// TTL
+				byte(ttl >> 24), byte(ttl >> 16), byte(ttl >> 8), byte(ttl),
+				// RDLENGTH
+				0x00, 0x04,
+				// RDATA
+				ip4[0], ip4[1], ip4[2], ip4[3],
+			}
+			dst = append(dst, answer[:]...)
+		} else {
+			_ = ip[15]
+			answer := [...]byte{
+				// NAME
+				0xc0, byte(offset),
+				// TYPE
+				0x00, byte(QTypeAAAA),
+				// CLASS
+				byte(req.Question.QClass >> 8), byte(req.Question.QClass),
+				// TTL
+				byte(ttl >> 24), byte(ttl >> 16), byte(ttl >> 8), byte(ttl),
+				// RDLENGTH
+				0x00, 0x10,
+				// RDATA
+				ip[0], ip[1], ip[2], ip[3],
+				ip[4], ip[5], ip[6], ip[7],
+				ip[8], ip[9], ip[10], ip[11],
+				ip[12], ip[13], ip[14], ip[15],
+			}
+			dst = append(dst, answer[:]...)
+		}
+	}
+
+	return dst
+}
