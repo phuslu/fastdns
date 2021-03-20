9d91d19b224444c681cc73d74967f5d2b327f9b3
phuslu <phus.lu@gmail.com>
2021-03-21 02:32:23 +0800

    add http handler

---
+++ http.go
@@ -0,0 +1,48 @@
+package fastdns
+
+import (
+	"net"
+	"net/http"
+	"strconv"
+)
+
+func HTTPHandlerFunc(h Handler) http.HandlerFunc {
+	return func(rw http.ResponseWriter, req *http.Request) {
+		b := AcquireByteBuffer()
+		defer ReleaseByteBuffer(b)
+
+		b.B = b.B[:cap(b.B)]
+		n, err := req.Body.Read(b.B)
+		if err != nil {
+			return
+		}
+		b.B = b.B[:n]
+
+		r := AcquireRequest()
+		defer ReleaseRequest(r)
+
+		err = ParseRequest(b.B, r)
+		if err != nil {
+			return
+		}
+
+		ip, port, _ := net.SplitHostPort(req.RemoteAddr)
+		addr := &net.TCPAddr{IP: net.ParseIP(ip)}
+		addr.Port, _ = strconv.Atoi(port)
+
+		h.ServeDNS(&httpResponseWriter{rw, addr}, r)
+	}
+}
+
+type httpResponseWriter struct {
+	rw   http.ResponseWriter
+	addr net.Addr
+}
+
+func (rw *httpResponseWriter) RemoteAddr() net.Addr {
+	return rw.addr
+}
+
+func (rw *httpResponseWriter) Write(p []byte) (n int, err error) {
+	return rw.rw.Write(p)
+}
