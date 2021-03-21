595c3910a70bce857f1d3ed59a9b4af3b6fbe0da
phuslu <phus.lu@gmail.com>
2021-03-21 18:36:29 +0800

    add main.go

---
+++ main.go
@@ -0,0 +1,43 @@
+// +build ignore
+
+package main
+
+import (
+	"log"
+	"net"
+	"os"
+
+	"github.com/phuslu/fastdns"
+)
+
+type DNSHandler struct {
+	Debug bool
+}
+
+func (h *DNSHandler) ServeDNS(rw fastdns.ResponseWriter, req *fastdns.Request) {
+	addr, name := rw.RemoteAddr(), req.GetDomainName()
+	if h.Debug {
+		log.Printf("%s] %s: CLASS %s TYPE %s\n", addr, name, req.Question.Class, req.Question.Type)
+	}
+
+	if req.Question.Type != fastdns.QTypeA {
+		fastdns.Error(rw, req, fastdns.NXDOMAIN)
+		return
+	}
+
+	fastdns.Host(rw, req, []net.IP{net.IP{8, 8, 8, 8}}, 300)
+}
+
+func main() {
+	server := &fastdns.Server{
+		Handler: &DNSHandler{
+			Debug: true,
+		},
+		Logger: log.New(os.Stderr, "", 0),
+	}
+
+	err := server.ListenAndServe(":53")
+	if err != nil {
+		log.Fatalf("dnsserver error: %+v", err)
+	}
+}
