5cbb1ed4869ff0be64506b7e4447a3d696f16828
phuslu <phus.lu@gmail.com>
2021-03-23 11:01:11 +0800

    add tests to handler

--- handler_test.go
+++ handler_test.go
@@ -20,7 +20,7 @@ func (rw *mockResponseWriter) Write(p []byte) (n int, err error) {
 	return
 }
 
-var handlerRequest = &Request{
+var mockHandlerRequest = &Request{
 	Header{
 		ID:      0x0002,
 		QR:      0x00,
@@ -43,6 +43,23 @@ var handlerRequest = &Request{
 	},
 }
 
+func TestHandlerResponseWriter(t *testing.T) {
+	rw := &responseWriter{
+		addr: &net.UDPAddr{IP: net.IP{1, 1, 1, 1}, Port: 53},
+	}
+
+	if s := rw.RemoteAddr().String(); s != "1.1.1.1:53" {
+		t.Errorf("response writer return error address: %+v", s)
+	}
+
+	var err error
+	rw.conn, err = net.DialUDP("udp", nil, rw.RemoteAddr().(*net.UDPAddr))
+	if err != nil {
+		t.Errorf("response writer dial udp error: %+v", err)
+	}
+	_, _ = rw.Write([]byte("test"))
+}
+
 func TestHandlerError(t *testing.T) {
 	var cases = []struct {
 		Hex   string
@@ -56,10 +73,125 @@ func TestHandlerError(t *testing.T) {
 
 	rw := &mockResponseWriter{}
 	for _, c := range cases {
-		Error(rw, handlerRequest, c.RCODE)
+		Error(rw, mockHandlerRequest, c.RCODE)
 		if got, want := hex.EncodeToString(rw.B), c.Hex; got != want {
 			t.Errorf("Error(%v) error got=%#v want=%#v", c.RCODE, got, want)
 		}
 	}
+}
 
+func TestHandlerHost(t *testing.T) {
+	var cases = []struct {
+		Hex string
+		IP  net.IP
+		TTL uint32
+	}{
+		{
+			"00028100000100010000000002686b0470687573026c750000010001c00c000100010000012c000401020408",
+			net.IP{1, 2, 4, 8},
+			300,
+		},
+	}
+
+	rw := &mockResponseWriter{}
+	for _, c := range cases {
+		Host(rw, mockHandlerRequest, []net.IP{c.IP}, c.TTL)
+		if got, want := hex.EncodeToString(rw.B), c.Hex; got != want {
+			t.Errorf("Host(%v) error got=%#v want=%#v", c.IP, got, want)
+		}
+	}
+}
+
+func TestHandlerCNAME(t *testing.T) {
+	var cases = []struct {
+		Hex   string
+		CNAME string
+		TTL   uint32
+	}{
+		{
+			"00028100000100010000000002686b0470687573026c750000010001c00c000500010000012c001305636e616d65076578616d706c6503636f6d00",
+			"cname.example.com",
+			300,
+		},
+	}
+
+	rw := &mockResponseWriter{}
+	for _, c := range cases {
+		CNAME(rw, mockHandlerRequest, []string{c.CNAME}, nil, c.TTL)
+		if got, want := hex.EncodeToString(rw.B), c.Hex; got != want {
+			t.Errorf("CNAME(%v) error got=%#v want=%#v", c.CNAME, got, want)
+		}
+	}
+}
+
+func TestHandlerSRV(t *testing.T) {
+	var cases = []struct {
+		Hex       string
+		SRV       string
+		Priovrity uint16
+		Weight    uint16
+		Port      uint16
+		TTL       uint32
+	}{
+		{
+			"00028100000100010000000002686b0470687573026c750000010001c00c002100010000012c001c03e803e81f41087365727669636531076578616d706c6503636f6d00",
+			"service1.example.com",
+			1000,
+			1000,
+			8001,
+			300,
+		},
+	}
+
+	rw := &mockResponseWriter{}
+	for _, c := range cases {
+		SRV(rw, mockHandlerRequest, c.SRV, c.Priovrity, c.Weight, c.Port, c.TTL)
+		if got, want := hex.EncodeToString(rw.B), c.Hex; got != want {
+			t.Errorf("SRV(%v) error got=%#v want=%#v", c.SRV, got, want)
+		}
+	}
+}
+
+func TestHandlerPTR(t *testing.T) {
+	var cases = []struct {
+		Hex string
+		PTR string
+		TTL uint32
+	}{
+		{
+			"00028100000100010000000002686b0470687573026c750000010001c00c000c00010000012c000f03707472076578616d706c65036f726700",
+			"ptr.example.org",
+			300,
+		},
+	}
+
+	rw := &mockResponseWriter{}
+	for _, c := range cases {
+		PTR(rw, mockHandlerRequest, c.PTR, c.TTL)
+		if got, want := hex.EncodeToString(rw.B), c.Hex; got != want {
+			t.Errorf("PTR(%v) error got=%#v want=%#v", c.PTR, got, want)
+		}
+	}
+}
+
+func TestHandlerTXT(t *testing.T) {
+	var cases = []struct {
+		Hex string
+		TXT string
+		TTL uint32
+	}{
+		{
+			"00028100000100010000000002686b0470687573026c750000010001c00c001000010000012c000d0d69616d617478747265636f7264",
+			"iamatxtrecord",
+			300,
+		},
+	}
+
+	rw := &mockResponseWriter{}
+	for _, c := range cases {
+		TXT(rw, mockHandlerRequest, c.TXT, c.TTL)
+		if got, want := hex.EncodeToString(rw.B), c.Hex; got != want {
+			t.Errorf("TXT(%v) error got=%#v want=%#v", c.TXT, got, want)
+		}
+	}
 }
