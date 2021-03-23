217f8fb37941cc06f4533f60fe63e5838001402c
phuslu <phus.lu@gmail.com>
2021-03-23 18:12:03 +0800

    add forkserver_test.go

---
+++ forkserver_test.go
@@ -0,0 +1,79 @@
+package fastdns
+
+import (
+	"context"
+	"log"
+	"net"
+	"os"
+	"runtime"
+	"testing"
+	"time"
+)
+
+func TestForkServerHost(t *testing.T) {
+	if runtime.GOOS == "windows" {
+		// On Windows, the resolver always uses C library functions, such as GetAddrInfo and DnsQuery.
+		return
+	}
+
+	s := &ForkServer{
+		Handler: &mockServerHandler{},
+		Logger:  log.New(os.Stdout, "", 0),
+	}
+
+	const addr = "127.0.0.1:5353"
+
+	go func() {
+		err := s.ListenAndServe(addr)
+		if err != nil {
+			t.Errorf("listen %+v error: %+v", addr, err)
+		}
+	}()
+
+	time.Sleep(100 * time.Millisecond)
+
+	resolver := &net.Resolver{
+		PreferGo: true,
+		Dial: func(context.Context, string, string) (net.Conn, error) {
+			return net.Dial("udp", addr)
+		},
+	}
+
+	ips, err := resolver.LookupHost(context.Background(), "example.org")
+	if err != nil {
+		t.Errorf("LookupHost return error: %+v", err)
+	}
+	if ips[0] != "1.1.1.1" {
+		t.Errorf("LookupHost return mismatched reply: %+v", ips)
+	}
+}
+
+func TestForkServerParseRequestError(t *testing.T) {
+	if runtime.GOOS == "windows" {
+		// On Windows, the resolver always uses C library functions, such as GetAddrInfo and DnsQuery.
+		return
+	}
+
+	s := &ForkServer{
+		Handler: &mockServerHandler{},
+		Logger:  log.New(os.Stdout, "", 0),
+	}
+
+	const addr = "127.0.0.1:5353"
+
+	go func() {
+		err := s.ListenAndServe(addr)
+		if err != nil {
+			t.Errorf("listen %+v error: %+v", addr, err)
+		}
+	}()
+
+	time.Sleep(100 * time.Millisecond)
+
+	conn, err := net.Dial("udp", addr)
+	if err != nil {
+		t.Errorf("dial to %+v return error: %+v", addr, err)
+	}
+
+	_, _ = conn.Write([]byte{0x00, 0x02, 0x01, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00})
+}
