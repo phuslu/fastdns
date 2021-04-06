8c35bc6e18519b3d2964b54b27387c72b2bfbfbd
phuslu <phus.lu@gmail.com>
2021-04-06 20:56:15 +0800

    add transport example

--- fastdns.go
+++ fastdns.go
@@ -9,12 +9,14 @@ import (
 	"net/http"
 	_ "net/http/pprof"
 	"os"
+	"strings"
 
 	"github.com/phuslu/fastdns"
 )
 
 type DNSHandler struct {
-	Debug bool
+	DNSTransport *fastdns.Transport
+	Debug        bool
 }
 
 func (h *DNSHandler) ServeDNS(rw fastdns.ResponseWriter, req *fastdns.Message) {
@@ -22,33 +24,53 @@ func (h *DNSHandler) ServeDNS(rw fastdns.ResponseWriter, req *fastdns.Message) {
 		log.Printf("%s] %s: CLASS %s TYPE %s\n", rw.RemoteAddr(), req.Domain, req.Question.Class, req.Question.Type)
 	}
 
-	switch req.Question.Type {
-	case fastdns.TypeA:
-		fastdns.HOST(rw, req, 300, []net.IP{{10, 0, 0, 1}})
-	case fastdns.TypeAAAA:
-		fastdns.HOST(rw, req, 300, []net.IP{net.ParseIP("2001:4860:4860::8888")})
-	case fastdns.TypeCNAME:
-		fastdns.CNAME(rw, req, 300, []string{"dns.google"}, []net.IP{{8, 8, 8, 8}, {8, 8, 4, 4}})
-	case fastdns.TypeNS:
-		fastdns.SRV(rw, req, 300, "www.google.com", 1000, 1000, 80)
-	case fastdns.TypeMX:
-		fastdns.NS(rw, req, 60, []string{"ns1.zdns.google", "ns2.zdns.google"})
-	case fastdns.TypeSOA:
-		fastdns.SOA(rw, req, 60, "ns1.google.com", "dns-admin.google.com", 1073741824, 900, 900, 1800, 60)
-	case fastdns.TypeSRV:
-		fastdns.MX(rw, req, 60, []fastdns.MXRecord{{10, "mail.gmail.com"}, {20, "smtp.gmail.com"}})
-	case fastdns.TypePTR:
-		fastdns.PTR(rw, req, 0, "ptr.example.com")
-	case fastdns.TypeTXT:
-		fastdns.TXT(rw, req, 300, "helloworld")
-	default:
-		fastdns.Error(rw, req, fastdns.RcodeNameError)
+	if strings.HasSuffix(string(req.Domain), ".google.com") {
+		switch req.Question.Type {
+		case fastdns.TypeA:
+			fastdns.HOST(rw, req, 60, []net.IP{{8, 8, 8, 8}})
+		case fastdns.TypeAAAA:
+			fastdns.HOST(rw, req, 60, []net.IP{net.ParseIP("2001:4860:4860::8888")})
+		case fastdns.TypeCNAME:
+			fastdns.CNAME(rw, req, 60, []string{"dns.google"}, []net.IP{{8, 8, 8, 8}, {8, 8, 4, 4}})
+		case fastdns.TypeNS:
+			fastdns.SRV(rw, req, 60, "www.google.com", 1000, 1000, 80)
+		case fastdns.TypeMX:
+			fastdns.NS(rw, req, 60, []string{"ns1.zdns.google", "ns2.zdns.google"})
+		case fastdns.TypeSOA:
+			fastdns.SOA(rw, req, 60, "ns1.google.com", "dns-admin.google.com", 1073741824, 900, 900, 1800, 60)
+		case fastdns.TypeSRV:
+			fastdns.MX(rw, req, 60, []fastdns.MXRecord{{10, "mail.gmail.com"}, {20, "smtp.gmail.com"}})
+		case fastdns.TypePTR:
+			fastdns.PTR(rw, req, 0, "ptr.google.com")
+		case fastdns.TypeTXT:
+			fastdns.TXT(rw, req, 60, "greetingfromgoogle")
+		default:
+			fastdns.Error(rw, req, fastdns.RcodeNameError)
+		}
 	}
+
+	resp := fastdns.AcquireMessage()
+	defer fastdns.ReleaseMessage(resp)
+
+	err := h.DNSTransport.RoundTrip(req, resp)
+	if err != nil {
+		fastdns.Error(rw, req, fastdns.RcodeServerFailure)
+	}
+
+	if h.Debug {
+		log.Printf("%s] %s: %s reply %d answers\n", rw.RemoteAddr(), req.Domain, h.DNSTransport.Address, resp.Header.ANCount)
+	}
+
+	rw.Write(resp.Raw)
 }
 
 func main() {
 	server := &fastdns.ForkServer{
 		Handler: &DNSHandler{
+			DNSTransport: &fastdns.Transport{
+				Address:  &net.UDPAddr{IP: net.IP{1, 1, 1, 1}, Port: 53},
+				MaxConns: 4096,
+			},
 			Debug: os.Getenv("DEBUG") != "",
 		},
 		ErrorLog: log.Default(),
