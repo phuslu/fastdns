b0361778d59a0a651668b96c8ba8cae79c509065
phuslu <phus.lu@gmail.com>
2022-08-01 22:46:17 +0800

    speedup by unsafe

---
+++ record_le.go
@@ -0,0 +1,127 @@
+//go:build 386 || amd64 || arm || amd64p32 || arm64 || ppc64le || mipsle || mips64le || mips64p32le
+// +build 386 amd64 arm amd64p32 arm64 ppc64le mipsle mips64le mips64p32le
+
+package fastdns
+
+import (
+	"net/netip"
+	"unsafe"
+)
+
+// AppendHOSTRecord appends the Host records to dst and returns the resulting dst.
+func AppendHOSTRecord(dst []byte, req *Message, ttl uint32, ips []netip.Addr) []byte {
+	for _, ip := range ips {
+		b := (*[16]byte)(unsafe.Pointer(&ip))
+		if ip.Is4() {
+			answer := [...]byte{
+				// NAME
+				0xc0, 0x0c,
+				// TYPE
+				0x00, byte(TypeA),
+				// CLASS
+				byte(req.Question.Class >> 8), byte(req.Question.Class),
+				// TTL
+				byte(ttl >> 24), byte(ttl >> 16), byte(ttl >> 8), byte(ttl),
+				// RDLENGTH
+				0x00, 0x04,
+				// RDATA
+				b[11], b[10], b[9], b[8],
+			}
+			dst = append(dst, answer[:]...)
+		} else {
+			answer := [...]byte{
+				// NAME
+				0xc0, 0x0c,
+				// TYPE
+				0x00, byte(TypeAAAA),
+				// CLASS
+				byte(req.Question.Class >> 8), byte(req.Question.Class),
+				// TTL
+				byte(ttl >> 24), byte(ttl >> 16), byte(ttl >> 8), byte(ttl),
+				// RDLENGTH
+				0x00, 0x10,
+				// RDATA
+				b[7], b[6], b[5], b[4],
+				b[3], b[2], b[1], b[0],
+				b[15], b[14], b[13], b[12],
+				b[11], b[10], b[9], b[8],
+			}
+			dst = append(dst, answer[:]...)
+		}
+	}
+
+	return dst
+}
+
+// AppendCNAMERecord appends the CNAME and Host records to dst and returns the resulting dst.
+func AppendCNAMERecord(dst []byte, req *Message, ttl uint32, cnames []string, ips []netip.Addr) []byte {
+	offset := 0x0c
+	// CName Records
+	for i, cname := range cnames {
+		// fixed size array for avoid bounds check
+		answer := [...]byte{
+			// NAME
+			0xc0 | byte(offset>>8), byte(offset),
+			// TYPE
+			0x00, byte(TypeCNAME),
+			// CLASS
+			byte(req.Question.Class >> 8), byte(req.Question.Class),
+			// TTL
+			byte(ttl >> 24), byte(ttl >> 16), byte(ttl >> 8), byte(ttl),
+			// RDLENGTH
+			0x00, byte(len(cname) + 2),
+		}
+		dst = append(dst, answer[:]...)
+		// set offset
+		if i == 0 {
+			offset += len(req.Question.Name) + 2 + 2
+		} else {
+			offset += len(cname) + 2
+		}
+		offset += len(answer)
+		// RDATA
+		dst = EncodeDomain(dst, cname)
+	}
+	// Host Records
+	for _, ip := range ips {
+		b := (*[16]byte)(unsafe.Pointer(&ip))
+		if ip.Is4() {
+			answer := [...]byte{
+				// NAME
+				0xc0 | byte(offset>>8), byte(offset),
+				// TYPE
+				0x00, byte(TypeA),
+				// CLASS
+				byte(req.Question.Class >> 8), byte(req.Question.Class),
+				// TTL
+				byte(ttl >> 24), byte(ttl >> 16), byte(ttl >> 8), byte(ttl),
+				// RDLENGTH
+				0x00, 0x04,
+				// RDATA
+				b[11], b[10], b[9], b[8],
+			}
+			dst = append(dst, answer[:]...)
+		} else {
+			answer := [...]byte{
+				// NAME
+				0xc0 | byte(offset>>8), byte(offset),
+				// TYPE
+				0x00, byte(TypeAAAA),
+				// CLASS
+				byte(req.Question.Class >> 8), byte(req.Question.Class),
+				// TTL
+				byte(ttl >> 24), byte(ttl >> 16), byte(ttl >> 8), byte(ttl),
+				// RDLENGTH
+				0x00, 0x10,
+				// RDATA
+				b[7], b[6], b[5], b[4],
+				b[3], b[2], b[1], b[0],
+				b[15], b[14], b[13], b[12],
+				b[11], b[10], b[9], b[8],
+			}
+			dst = append(dst, answer[:]...)
+		}
+	}
+
+	return dst
+}
