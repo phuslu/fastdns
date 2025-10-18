f33af0d60c08ca4ab57157e8e54b91de8564879c
phuslu <phus.lu@gmail.com>
2025-10-18 23:50:02 +0800

    simplify appendable bytes

--- util.go
+++ util.go
@@ -1,10 +1,6 @@
 package fastdns
 
 import (
-	"encoding/base64"
-	"encoding/hex"
-	"net/netip"
-	"strconv"
 	"unsafe"
 )
 
@@ -17,87 +13,3 @@ func b2s(b []byte) string { return *(*string)(unsafe.Pointer(&b)) }
 //go:noescape
 //go:linkname cheaprandn runtime.cheaprandn
 func cheaprandn(x uint32) uint32
-
-/*
-	  b := appendablebytes(make([]byte, 0, 1024))
-	  b = b.Str("GET ").Str(req.RequestURI).Str(" HTTP/1.1\r\n")
-	  for key, values := range req.Header {
-		for _, value := range values {
-			b = b.Str(key).Str(": ").Str(value).Str("\r\n")
-	  	}
-	  }
-	  b = b.Str("\r\n")
-*/
-type appendablebytes []byte
-
-// Str appends s to the buffer.
-func (b appendablebytes) Str(s string) appendablebytes {
-	return append(b, s...)
-}
-
-// Bytes appends raw bytes to the buffer.
-func (b appendablebytes) Bytes(s []byte) appendablebytes {
-	return append(b, s...)
-}
-
-// Byte appends a single byte to the buffer.
-func (b appendablebytes) Byte(c byte) appendablebytes {
-	return append(b, c)
-}
-
-// Base64 appends the base64 encoding of data to the buffer.
-func (b appendablebytes) Base64(data []byte) appendablebytes {
-	return base64.StdEncoding.AppendEncode(b, data)
-}
-
-// Hex appends the hexadecimal encoding of data to the buffer.
-func (b appendablebytes) Hex(data []byte) appendablebytes {
-	return hex.AppendEncode(b, data)
-}
-
-// NetIPAddr appends the textual representation of ip to the buffer.
-func (b appendablebytes) NetIPAddr(ip netip.Addr) appendablebytes {
-	return ip.AppendTo(b)
-}
-
-// NetIPAddrPort appends the textual representation of addr to the buffer.
-func (b appendablebytes) NetIPAddrPort(addr netip.AddrPort) appendablebytes {
-	return addr.AppendTo(b)
-}
-
-// Uint64 appends i formatted in the specified base.
-func (b appendablebytes) Uint64(i uint64, base int) appendablebytes {
-	return strconv.AppendUint(b, i, base)
-}
-
-// Float64 appends f using decimal notation.
-func (b appendablebytes) Float64(f float64) appendablebytes {
-	return strconv.AppendFloat(b, f, 'f', -1, 64)
-}
-
-// Int64 appends i formatted in the specified base.
-func (b appendablebytes) Int64(i int64, base int) appendablebytes {
-	return strconv.AppendInt(b, i, base)
-}
-
-// Pad right-aligns the buffer to the base by inserting the provided byte.
-func (b appendablebytes) Pad(c byte, base int) appendablebytes {
-	n := (base - len(b)%base) % base
-	if n == 0 {
-		return b
-	}
-	if n <= 32 {
-		b = append(b, make([]byte, 32)...)
-		b = b[:len(b)+n-32]
-	} else {
-		b = append(b, make([]byte, n)...)
-	}
-	if c != 0 {
-		m := len(b) - 1
-		_ = b[m]
-		for i := m - n + 1; i <= m; i++ {
-			b[i] = c
-		}
-	}
-	return b
-}
